7e2da18ef1814172877b18347886d00018e8b834
abmah <ab.mahasnh@gmail.com>
2022-09-21 00:46:42 +0300

    new cursor

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
 import PageHeader from "./components/Header/PageHeader";
-import Body from "./components/Body/BodyPage";
 import Cursor from "./components/Cursor/Cursor";
 import Test from "./components/Test/TestPage";
 import SkillSet from "./components/Cards/SkillSet";
 import Projects from "./components/Projects/Projects";
 import Contact from "./components/Contact/ContactPage";
 import About from "./components/About/About";
+import React, { useState, useEffect } from "react";
 function App() {
+  const [isDesktop, setDesktop] = useState(window.innerWidth > 650);
+
+  const updateMedia = () => {
+    setDesktop(window.innerWidth > 650);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", updateMedia);
+    return () => window.removeEventListener("resize", updateMedia);
+  });
+
   return (
     <div className="App">
       <Cursor />
-
       <PageHeader />
+
       <Test />
+
+      {/* <Test /> */}
       <About />
       <SkillSet />
       <Projects />
--- src/components/Cursor/Cursor.js
+++ src/components/Cursor/Cursor.js
@@ -1,93 +1,112 @@
-import { useRef, useEffect } from "react";
-import styles from "./cursor.module.css";
-
+// import { useRef, useEffect } from "react";
+// import styles from "./cursor.module.css";
+import AnimatedCursor from "react-animated-cursor";
 export default function Cursor() {
-  const positionRef = useRef({
-    mouseX: 0,
-    mouseY: 0,
-    destinationX: 0,
-    destinationY: 0,
-    distanceX: 0,
-    distanceY: 0,
-    key: -1,
-  });
+  // const positionRef = useRef({
+  //   mouseX: 0,
+  //   mouseY: 0,
+  //   destinationX: 0,
+  //   destinationY: 0,
+  //   distanceX: 0,
+  //   distanceY: 0,
+  //   key: -1,
+  // });
 
-  const mainCursor = useRef(null);
-  const secondaryCursor = useRef(null);
+  // const mainCursor = useRef(null);
+  // const secondaryCursor = useRef(null);
 
-  useEffect(() => {
-    document.addEventListener("mousemove", (event) => {
-      const { clientX, clientY } = event;
+  // useEffect(() => {
+  //   document.addEventListener("mousemove", (event) => {
+  //     const { clientX, clientY } = event;
 
-      const mouseX = clientX;
-      const mouseY = clientY;
-      //@ts-ignore
-      positionRef.current.mouseX =
-        //@ts-ignore
-        mouseX - secondaryCursor.current.clientWidth / 2;
-      positionRef.current.mouseY =
-        //@ts-ignore
-        mouseY - secondaryCursor.current.clientHeight / 2;
-      //@ts-ignore
-      mainCursor.current.style.transform = `translate3d(${
-        //@ts-ignore
-        mouseX - mainCursor.current.clientWidth / 2
-        // @ts-ignore
-      }px, ${mouseY - mainCursor.current.clientHeight / 2}px, 0)`;
-    });
+  //     const mouseX = clientX;
+  //     const mouseY = clientY;
+  //     //@ts-ignore
+  //     positionRef.current.mouseX =
+  //       //@ts-ignore
+  //       mouseX - secondaryCursor.current.clientWidth / 2;
+  //     positionRef.current.mouseY =
+  //       //@ts-ignore
+  //       mouseY - secondaryCursor.current.clientHeight / 2;
+  //     //@ts-ignore
+  //     mainCursor.current.style.transform = `translate3d(${
+  //       //@ts-ignore
+  //       mouseX - mainCursor.current.clientWidth / 2
+  //       // @ts-ignore
+  //     }px, ${mouseY - mainCursor.current.clientHeight / 2}px, 0)`;
+  //   });
 
-    return () => {};
-  }, []);
-  useEffect(() => {
-    const followMouse = () => {
-      positionRef.current.key = requestAnimationFrame(followMouse);
-      const {
-        mouseX,
-        mouseY,
-        destinationX,
-        destinationY,
-        distanceX,
-        distanceY,
-      } = positionRef.current;
-      if (!destinationX || !destinationY) {
-        positionRef.current.destinationX = mouseX;
-        positionRef.current.destinationY = mouseY;
-      } else {
-        positionRef.current.distanceX = (mouseX - destinationX) * 0.1;
-        positionRef.current.distanceY = (mouseY - destinationY) * 0.1;
-        if (
-          Math.abs(positionRef.current.distanceX) +
-            Math.abs(positionRef.current.distanceY) <
-          0.1
-        ) {
-          positionRef.current.destinationX = mouseX;
-          positionRef.current.destinationY = mouseY;
-        } else {
-          positionRef.current.destinationX += distanceX;
-          positionRef.current.destinationY += distanceY;
-        }
-      }
-      //@ts-ignore
-      secondaryCursor.current.style.transform = `translate3d(${destinationX}px, ${destinationY}px, 0)`;
-    };
-    followMouse();
-  }, []);
-  useEffect(() => {
-    window.addEventListener("change", () => {
-      const vw = Math.max(
-        document.documentElement.clientWidth || 0,
-        window.innerWidth || 0
-      );
-      console.log(vw);
-    });
-  }, []);
+  //   return () => {};
+  // }, []);
+  // useEffect(() => {
+  //   const followMouse = () => {
+  //     positionRef.current.key = requestAnimationFrame(followMouse);
+  //     const {
+  //       mouseX,
+  //       mouseY,
+  //       destinationX,
+  //       destinationY,
+  //       distanceX,
+  //       distanceY,
+  //     } = positionRef.current;
+  //     if (!destinationX || !destinationY) {
+  //       positionRef.current.destinationX = mouseX;
+  //       positionRef.current.destinationY = mouseY;
+  //     } else {
+  //       positionRef.current.distanceX = (mouseX - destinationX) * 0.1;
+  //       positionRef.current.distanceY = (mouseY - destinationY) * 0.1;
+  //       if (
+  //         Math.abs(positionRef.current.distanceX) +
+  //           Math.abs(positionRef.current.distanceY) <
+  //         0.1
+  //       ) {
+  //         positionRef.current.destinationX = mouseX;
+  //         positionRef.current.destinationY = mouseY;
+  //       } else {
+  //         positionRef.current.destinationX += distanceX;
+  //         positionRef.current.destinationY += distanceY;
+  //       }
+  //     }
+  //     //@ts-ignore
+  //     secondaryCursor.current.style.transform = `translate3d(${destinationX}px, ${destinationY}px, 0)`;
+  //   };
+  //   followMouse();
+  // }, []);
 
   return (
     <>
-      <div ref={mainCursor} className={styles.mainCursor}></div>
+      {/* <div ref={mainCursor} className={styles.mainCursor}></div>
       <div ref={secondaryCursor} className={styles.secondaryCursor}>
         <div className={styles.cursorbackground}></div>
-      </div>
+      </div> */}
+
+      <AnimatedCursor
+        innerSize={20}
+        outerSize={0}
+        outerAlpha={0.7}
+        innerScale={0.4}
+        outerStyle={{
+          backgroundColor: "pink",
+        }}
+        innerStyle={{
+          backgroundColor: "white",
+          mixBlendMode: "difference",
+        }}
+        outerScale={5}
+        clickables={[
+          "a",
+          'input[type="text"]',
+          'input[type="email"]',
+          'input[type="number"]',
+          'input[type="submit"]',
+          'input[type="image"]',
+          "label[for]",
+          "select",
+          "textarea",
+          "button",
+          ".link",
+        ]}
+      />
     </>
   );
 }
