084cc8a6e8df4e8c81bcf84ce5fc43c739448fec
abmah <ab.mahasnh@gmail.com>
2022-09-20 23:08:54 +0300

    fixed scrolling

--- src/components/Header/PageHeader.js
+++ src/components/Header/PageHeader.js
@@ -52,8 +52,8 @@ export default function PageHeader() {
         }
       >
         <ul className={styles.MenuList}>
-          <li onClick={() => scrollToProjects(3610)}>projects</li>
-          <li onClick={() => scrollToProjects(5010)}>contact</li>
+          <li onClick={() => scrollToProjects(4810)}>projects</li>
+          <li onClick={() => scrollToProjects(6310)}>contact</li>
         </ul>
       </div>
     </div>
